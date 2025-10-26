37dd4e1c974940531e1c4db9e69bcaa684de9ef9
Shaik Sadath <sadath.sa@gmail.com>
2025-10-26 23:06:23 +0530

    Add Prisma setup and initial database schema
    - Added Prisma configuration in `prisma.config.ts`.
    - Created initial database schema in `schema.prisma` with User and Post models.
    - Generated migration files for the initial database setup.
    - Integrated Prisma client in the application with a new `db.ts` file.
    - Updated the main page to fetch and display users from the database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/ui/button'
+import prisma from '@/lib/db'
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany()
   return (
     <div>
       <Button variant={'outline'}>Click Mme </Button>
+      {JSON.stringify(users)}
     </div>
   )
 }
