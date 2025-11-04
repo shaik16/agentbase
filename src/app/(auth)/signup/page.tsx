9bf83e459bd0ef637d1c76de1504f8c36b974118
Shaik Sadath <sadath.sa@gmail.com>
2025-11-05 00:28:28 +0530

    feat: add polar payments

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,4 +1,4 @@
-import { SignupForm } from '@/app/features/auth/components/signup-form'
+import { SignupForm } from '@/features/auth/components/signup-form'
 import { requireUnAuth } from '@/lib/auth-utils'
 
 const Page = async () => {
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { polar, portal, checkout } from '@polar-sh/better-auth'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from './db'
+import { polarClient } from './polar'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +12,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: '5c105566-2918-4722-ac83-4609d2419f77',
+              slug: 'pro',
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 })
